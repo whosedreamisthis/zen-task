8a2bdd16130f0e8b88c4db3b953e65441811c4e9
Dana Sharon <whosedreamisthis@gmail.com>
2026-02-05 14:44:40 -0800

    edit list name not persistant yet

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,18 +1,58 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import { TaskListType, TaskType } from '@/types/types';
 import TaskCard from './TaskCard';
 import { Input } from './ui/input';
-import { Plus } from 'lucide-react';
+import { Check, Divide, Plus, X } from 'lucide-react';
 import { Button } from './ui/button';
 import { Menu } from 'lucide-react';
 export default function TaskList({ taskList }: { taskList: TaskListType }) {
+	const [editingName, setEditingName] = useState(false);
+	const [listName, setListName] = useState('');
+
+	const cancelNameEdit = () => {
+		// Here you would eventually save tempName to your state/database
+		setEditingName(false);
+		setListName('');
+	};
+
+	const saveNameEdit = () => {
+		// Here you would eventually save tempName to your state/database
+		setEditingName(false);
+		setListName('');
+	};
+
 	return (
 		<>
 			<div className="relative bg-gray-100 w-[400px] m-auto p-4">
 				<Button variant="ghost" className="absolute top-3 left-1">
 					<Menu className="text-center" />
 				</Button>
-				<h1 className="text-center font-bold">{taskList.name}</h1>
+				{editingName ? (
+					<div className="flex items-center gap-2 ml-8">
+						<Input
+							value={listName}
+							autoFocus
+							onBlur={saveNameEdit}
+							onChange={(e) => {
+								setListName(e.target.value);
+							}}
+							placeholder={taskList.name}
+						/>
+						<Check
+							className="text-green-400 font-bold"
+							onClick={saveNameEdit}
+						/>
+						<X onClick={cancelNameEdit} />
+					</div>
+				) : (
+					<h1
+						className="text-center font-bold"
+						onClick={() => setEditingName(true)}
+					>
+						{taskList.name}
+					</h1>
+				)}
 			</div>
 			<div className="flex flex-col gap-4 w-[400px] m-auto mt-10">
 				<div className="flex justify-center items-center">
